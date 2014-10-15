9344ca14882e51cacfaaa77c5be7441a94c8f817
Taylor Ledingham <taylor.ledingham@gmail.com>
2014-10-15 14:08:07 -0700

    Initial Commit

---
+++ TLSimonSays/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TLSimonSays
+//
+//  Created by Taylor Ledingham on 2014-10-15.
+//  Copyright (c) 2014 Taylor Ledingham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TLSimonSays/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TLSimonSays
+//
+//  Created by Taylor Ledingham on 2014-10-15.
+//  Copyright (c) 2014 Taylor Ledingham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
